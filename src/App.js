463b46a23c8ebd9103febde7cf27f07d587cf177
Suraj garg <garg69850@gmail.com>
2021-09-12 23:06:02 +0530

    Basic newsapp is ready with new features

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import Navbar from './components/Navbar';
 import News from './components/News';
+import LoadingBar from 'react-top-loading-bar';
 
 export default class App extends Component {
-  c = 'John'
+
+  state = {
+    progress: 0
+  }
+
+  setProgress = (progress) => {
+    this.setState({ progress: progress });
+  }
   render() {
     return (
-      <div>
-        <Navbar />
-        {/* hello my first class based components {this.c} */}
-        <News />
-      </div>
+      <Router>
+        <div>
+          <LoadingBar
+            color='#f11946'
+            progress={this.state.progress}
+            height={3}
+          />
+          <Navbar />
+          <Switch>
+            {/* business entertainment general health science sports technology */}
+            <Route exact path="/"><News setProgress={this.setProgress} key="general" pageSize={6} category='general' /></Route>
+            <Route exact path="/entertainment"><News setProgress={this.setProgress} key="entertainment" pageSize={6} category='entertainment' /></Route>
+            <Route exact path="/business"><News setProgress={this.setProgress} key="business" pageSize={6} category='business' /></Route>
+            <Route exact path="/health"><News setProgress={this.setProgress} key="health" pageSize={6} category='health' /></Route>
+            <Route exact path="/sports"><News setProgress={this.setProgress} key="sports" pageSize={6} category='sports' /></Route>
+            <Route exact path="/science"><News setProgress={this.setProgress} key="science" pageSize={6} category='science' /></Route>
+            <Route exact path="/technology"><News setProgress={this.setProgress} key="technology" pageSize={6} category='technology' /></Route>
+          </Switch>
+        </div>
+      </Router>
     )
   }
 }
--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react'
 
 export class News extends Component {
     render() {
-        let { title, description, imageUrl } = this.props;
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
         return (
             <div>
-                <div className="card m-3" style={{ width: '18rem' }}>
+                <div className="card m-3" style={{ width: '20rem' }}>
+                    <span className="position-absolute bg-danger badge text-white">
+                        {source}
+                    </span>
                     <img src={imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
-                        {/* <h5 className="card-title">{this.props.title}</h5> */}
                         <p className="card-text">{description}</p>
-                        <a href="/newsdetail" className="btn btn-sm btn-primary">Go somewhere</a>
+                        <p className="card-text"><small className="text-muted">By {author ? author : 'unknown'} at {new Date(date).toDateString()}  {
+                            new Date(date).toLocaleTimeString()}</small></p>
+                        <a rel="noreferrer" target='_blank' href={newsUrl} className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
             </div>
